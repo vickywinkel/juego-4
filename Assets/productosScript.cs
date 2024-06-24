9a90525ff9c1f79521d64d9c72a8de0c762289be
vickywinkel <152075054+vickywinkel@users.noreply.github.com>
2024-06-24 14:32:20 -0300

    Inital commit
    commit inicial y la creacion de los scrips + la asigancion de cada uno

---
+++ Assets/productosScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class productosScript : MonoBehaviour
+{
+    public GameObject[] objetos;
+    private int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < objetos.Length; i++)
+        {
+            objetos[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
