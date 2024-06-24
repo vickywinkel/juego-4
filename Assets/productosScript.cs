7054f1eeeaf2d9cc6925e99359825c21058dca99
vickywinkel <vickywinkel@gmail.com>
2024-06-24 17:54:58 -0300

    Commit precio del que aparece

--- Assets/productosScript.cs
+++ Assets/productosScript.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class productosScript : MonoBehaviour
 {
     public GameObject[] objetos;
     public GameObject objetoActivado;
+    public objetoScript producto;
+    public Text txtPrecio;
+    public Vector3 coordentadas = new Vector3(-9, 2, 5);
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +22,16 @@ public class productosScript : MonoBehaviour
         int azar = Random.Range(0, objetos.Length);
         objetoActivado = objetos[azar];
         objetoActivado.SetActive(true);
+        objetoActivado.transform.position = coordentadas;
+
+        producto = objetoActivado.GetComponent<objetoScript>();
+        txtPrecio.text = ("$" + producto.precio);
+        Debug.Log (producto.precio);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
